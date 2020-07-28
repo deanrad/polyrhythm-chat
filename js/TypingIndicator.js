a56880b27974ecf1aabfcb7c3d3dae9e8e9637f6
Dean Radcliffe <deanmisc@yahoo.com>
2020-07-27 22:43:13 -0500

    2.3 - done!

--- js/TypingIndicator.js
+++ js/TypingIndicator.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
+import { concat, after, useListener } from "polyrhythm";
 
 export const TypingIndicator = ({ timeout = 5000 }) => {
   const [isTyping, setTyping] = useState(false);
+
+  const autoTimeoutTyper = () =>
+    concat(
+      after(0, () => setTyping(true)),
+      after(timeout, () => setTyping(false))
+    );
+
+  useListener(/message\/edit\/(?!me)/, autoTimeoutTyper, { mode: "replace" });
+  useListener(/message\/from/, () => setTyping(false));
 
   return (
     isTyping && (
