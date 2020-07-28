df910864d770875adb53b46050c0fd8a513580bb
Dean Radcliffe <deanmisc@yahoo.com>
2020-07-27 22:43:13 -0500

    2.3 - done!

--- js/index.js
+++ js/index.js
@@ -6,11 +6,11 @@ import { TypingIndicator } from "./TypingIndicator";
 import { MessageComposer } from "./MessageComposer";
 import { WebsocketService } from "./WebsocketService";
 import { ChatLog } from "./ChatLog";
-import { filter, trigger, after } from "polyrhythm";
+import { filter, trigger, after, concat } from "polyrhythm";
 
 // log all messages on the channel/event bus
 filter(true, ({ type, payload }) => console.log(type, payload));
-Object.assign(window, { trigger });
+Object.assign(window, { trigger, after });
 
 const root = document.querySelector(".chat-container");
 const app = (
@@ -23,11 +23,23 @@ const app = (
 );
 ReactDOM.render(app, root);
 
-after(1000).then(() => {
-  [
-    { text: "Hi YOU!" },
-    { text: "I'm here to talk - whaddya wanna know?" },
-    { text: "What is the answer to the eternal question?", userId: "me" },
-    { text: "It is ..." },
-  ].map((message) => trigger("message/create", message));
-});
+runStartupScript();
+/*
+  // Can also use trigger and after to run ad-hoc scripts in the console via:
+  trigger('message/edit/bot');
+  await after(1000, () => trigger('message/from/bot', { text: 'Done.' }));
+  await after(2000, () => trigger('message/edit/bot'))
+*/
+
+//prettier-ignore
+function runStartupScript() {
+  concat(
+    after(100, ["message/from/bot", "Hi YOU!"]),
+    after(100, ["message/from/bot", "I'm here to talk - whaddya wanna know?"]),
+    after(1500, ["message/edit/me"]),
+    after(1500, ["message/create", "What is the answer to the eternal question?", "me"]),
+    after(1500, ["message/edit/bot"]),
+    after(1500, ["message/from/bot", "It is ..."]),
+    after(500, ["message/edit/bot"])
+  ).subscribe(([type, text, userId]) => trigger(type, { text, userId }));
+}
